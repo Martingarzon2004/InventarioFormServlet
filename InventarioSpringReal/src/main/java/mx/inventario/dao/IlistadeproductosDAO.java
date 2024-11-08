9d4b6fe68a3166b9df97610e10bec3bed8527e0f
Martingarzon2004 <141690600+Martingarzon2004@users.noreply.github.com>
2024-11-08 11:24:12 -0500

    Delete InventarioSpringReal directory

--- InventarioSpringReal/src/main/java/mx/inventario/dao/IlistadeproductosDAO.java
+++
@@ -1,8 +0,0 @@
-package mx.inventario.dao;
-
-import com.inventario.domain.Listadeproducos;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IlistadeproductosDAO extends CrudRepository<Listadeproducos, Long>{
-    
-}
